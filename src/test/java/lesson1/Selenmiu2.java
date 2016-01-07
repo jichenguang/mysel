4d3ec1d5b8fbc9d426861e79375c84bff3c78bb0
JiChenguang <jichenguang@700bike.com>
2016-01-07 21:19:13 +0800

    testcore

---
+++ src/test/java/lesson1/Selenmiu2.java
@@ -0,0 +1,69 @@
+package lesson1;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+
+public class Selenmiu2 { 
+	
+	
+	@Test       
+
+           public static   String click(String username, String password) {
+																					
+					        WebDriver Driver = new FirefoxDriver();
+					        Driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+					        Driver.get("http://www.700store.com/login");
+							
+					      //因为淘宝的登录其实是嵌入在一个iframe里面的，所以得先切换到iframe中再操作
+					        Driver.switchTo().frame(0);
+
+					//输入用户名
+					        Driver.findElement(By.id("TPL_username_1")).clear();
+					        Driver.findElement(By.id("TPL_username_1")).sendKeys(username);
+
+					//输入密码
+					        Driver.findElement(By.id("TPL_password_1")).clear();
+					        Driver.findElement(By.id("TPL_password_1")).sendKeys(password);
+
+					//点击登录按钮
+					        Driver.findElement(By.id("J_SubmitStatic")).click();
+					        Driver.switchTo().defaultContent();
+					        try {
+
+					    //不停的检测，一旦当前页面URL不是登录页面URL，就说明浏览器已经进行了跳转
+					            while (true) {
+					                Thread.sleep(500L);
+					                if (!Driver.getCurrentUrl().startsWith("http://www.700store.com/login")) {
+					                    break;
+					                }
+					            }
+					        } catch (InterruptedException e) {
+					            e.printStackTrace();
+					        }
+					      //获取cookie，上面一跳出循环我认为就登录成功了，当然上面的判断不太严格，可以再进行修改
+					        Set<Cookie> cookies = Driver.manage().getCookies();
+					        String cookieStr = "";
+					        for (Cookie cookie : cookies) {
+					            cookieStr += cookie.getName() + "=" + cookie.getValue() + "; ";
+					        }
+
+					//退出，关闭浏览器
+					        Driver.quit();
+					        return cookieStr;
+					    
+							
+
+							} 
+}
---
+++ src/test/java/lesson1/testBaidu.java
@@ -0,0 +1,52 @@
+package lesson1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class testBaidu {
+	
+	
+	
+	@Test
+	/**
+	 * 打开google搜索百度
+	 * 
+	 * @param queryStr
+	 */
+	public    void demo() {
+		String url = "http://www.baidu.com";
+
+		WebDriver webDriver = new FirefoxDriver();
+		// 打开google
+		webDriver.get(url);
+
+		// 使用Selenium的dom模型获取form
+		WebElement webElement = webDriver.findElement(By.name("wd"));
+		webElement.sendKeys("baidu");
+		webElement.submit();
+
+		// 通过判断 title 内容等待搜索页面加载完毕，Timeout 设置10秒
+
+		(new WebDriverWait(webDriver, 30)).until(new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver d) {
+				return d.getTitle().toLowerCase().indexOf("百度_百度搜索") != -1;
+			}
+		});
+		
+		
+
+		String responseBody = webDriver.getPageSource();
+		System.out.println("Response : " + responseBody);
+
+		// 关闭浏览器
+		webDriver.close();
+	}
+
+}
